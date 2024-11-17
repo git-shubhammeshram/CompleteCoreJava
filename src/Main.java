7166fb84287766083bd8536a14689bccfa1168f2
git-shubhammeshram <shubhammeshram0999@gmail.com>
2024-11-17 15:08:23 +0530

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		/* Java 8 has the following main features
+		* 1.Lambda expressions
+		* 2.Functional interfaces
+		* 3.Predefined functional interfaces 1.Predicate 2.Function 3.Consumer 4.Supplier
+		* 4.Default and Static methods in interface
+		* 5.Method and Constructor references ::
+		* 6.Data And Time API
+		* 7.Base 64 incode and decode
+		* 8.Optional class
+		* 9.Stream API
+		* 10.Collectors class
+		* 11.ForEach method
+		* 12.Parallel array sorting
+		* 13.Nashorn javascript
+		* 14.Type and repeating annotation
+		* 15.IO, Concurrency, JDBC enhancements
+		*
+		*/
 	}
 }
